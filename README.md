7af772693d8b84cb7d50b506619395a753f4f983
Ikhlas Bano <98226842+ikhlasafzaal@users.noreply.github.com>
2024-04-08 03:42:10 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+
+# Currency Converter (TypeScript & Node.js)
+
+This command-line application, built with TypeScript and Node.js, enables users to convert between various currencies using real-time exchange rates.
+
+## Features
+
+- **Currency Conversion**: Convert between different currencies including USD, EUR, INR, PKR, GBP, and AUD.
+- **Interactive Interface**: Users can easily input their currency conversion preferences through an interactive command-line interface.
+- **Accurate Rates**: Current exchange rates are fetched to ensure accurate conversions.
+
+## Prerequisites
+
+- Node.js installed on your machine.
+- npm (Node Package Manager) installed on your machine.
+
+## Installation
+
+To install the Currency Converter CLI, use npm:
+
+```bash
+npm install -g @ikhlasbano/currency-converter
+```
+
+## Usage
+
+To use the Currency Converter CLI, run the following command:
+
+```bash
+currency-converter
+```
+
+Follow the prompts to select the source and target currencies, enter the amount, and view the converted value.
+
+
+## Accessing the Package on NPM
+
+You can access the package [here](https://www.npmjs.com/package/@ikhlasbano/currency-converter).
+
+
+
+Feel free to customize the README further to add any additional information or instructions you think would be helpful for users. Happy currency conversion!
---
+++ index.ts
@@ -0,0 +1,88 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+// const currency:any = {
+//     USD: 1, // base currency
+//     EUR: 0.91,
+//     INR: 73.65,
+//     PKR: 280,
+//     GBP: 0.73,
+//     AUD: 1.29
+// };
+
+const currency: any = {
+    PKR: 280, // base currency
+    USD: (1 / 280).toFixed(3),
+    EUR: (0.91 / 280).toFixed(3),
+    INR: (73.65 / 280).toFixed(3),
+    GBP: (0.73 / 280).toFixed(3),
+    AUD: (1.29 / 280).toFixed(3)
+};
+
+console.log(chalk.bold.bgBlueBright('\n \t Currency Converter'));
+
+let userReply = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter From Currency: ",
+        type: "list",
+        choices: ['USD', 'PKR', 'EUR', 'INR', 'GBP', 'AUD']
+    },
+    {
+        name: "to",
+        message: "Enter To Currency: ",
+        type: "list",
+        choices: ['USD', 'PKR', 'EUR', 'INR', 'GBP', 'AUD'],
+    }
+]);
+
+// console.log(userReply);
+
+if (userReply.from === userReply.to) {
+    console.log(chalk.red.bold("Error: Please select different currencies for conversion."));
+}
+
+else {
+    let userReplyAmount = await inquirer.prompt(
+        {
+            name: "userAmount",
+            message: "Enter your Amount: ",
+            type: "number",
+        }
+    );
+
+    // Fetching the conversion rate of the selected currencies
+    let fromAmount = currency[userReply.from];
+    let toAmount = currency[userReply.to];
+    let amount = userReplyAmount.userAmount;
+
+    console.log("From Currency Rate: " + fromAmount);
+    console.log("To Currency Rate: " + toAmount);
+    console.log("Amount: " + amount);
+
+
+    let baseAmount = amount / fromAmount;   //converted amount in base currency (From)
+    let finalAmount = baseAmount * toAmount;    //final converted amount
+
+    if (!isFinite(finalAmount)) {
+        console.log(chalk.red.bold("Error: Please Insert Valid Number! "));
+    }
+
+    else if (userReplyAmount.userAmount <= 0) {
+        console.log(chalk.red.bold("Error: Please enter a valid positive number for the amount."));
+    }
+
+    else {
+        console.log(chalk.bgGrey.bold(' \t Final Converted Value: '));
+
+        console.log(chalk.green.bold(`${amount} ${userReply.from} is equal to ${Math.floor(finalAmount)} ${userReply.to}`));
+    }
+
+}
+
+
+
+
+
